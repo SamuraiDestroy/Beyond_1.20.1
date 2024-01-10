7f040d9249e37811feb3b9496b56b848815583fa
SamuraiDestroy <01PMvictor@gmail.com>
2024-01-10 20:33:47 +0100

    Creative tab.

--- src/main/java/net/samuraidestroy/beyond/Beyond.java
+++ src/main/java/net/samuraidestroy/beyond/Beyond.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.samuraidestroy.beyond.item.ModCreativeModeTabs;
 import net.samuraidestroy.beyond.item.ModItems;
 import org.slf4j.Logger;
 
@@ -30,6 +31,8 @@ public class Beyond
     public Beyond()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
 
@@ -54,11 +57,11 @@ public class Beyond
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(ModItems.SOUL_SHARD);
-            event.accept(ModItems.SOUL_GEM);
-            event.accept(ModItems.SOUL_STEEL);
-        }
+//        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+//            event.accept(ModItems.SOUL_SHARD);
+//            event.accept(ModItems.SOUL_GEM);
+//            event.accept(ModItems.SOUL_STEEL);
+//        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/samuraidestroy/beyond/item/ModCreativeModeTabs.java
@@ -0,0 +1,22 @@
+package net.samuraidestroy.beyond.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.samuraidestroy.beyond.Beyond;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Beyond.MOD_ID);
+    public static final RegistryObject<CreativeModeTab> CHAPTER_I = CREATIVE_MODE_TABS.register("chapter_i_tab", () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SOUL_GEM.get())).title(Component.translatable("creativetab.chapter_i_tab")).displayItems((itemDisplayParameters, output) -> {
+        output.accept(ModItems.SOUL_SHARD.get());
+        output.accept(ModItems.SOUL_GEM.get());
+        output.accept(ModItems.SOUL_STEEL.get());
+    }).build());
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
